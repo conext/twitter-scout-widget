2bed80ba9797ae05797e320dc8d2a5a96455061f
Maciej Machulak <maciej.machulak@gmail.com>
2013-05-22 22:54:58 +0200

    group switching

--- scriptage.js
+++ scriptage.js
@@ -1,3 +1,4 @@
+var current_group;
 var response;
 var dbg;
 
@@ -84,8 +85,31 @@ function render_results(data) {
     clog("exiting redner_results()");
 }
 
+function conjure_tweet_button(ht) {
+    var proto_1 = "<a href='https://twitter.com/intent/tweet?button_hashtag=";
+    var proto_2 = "' class='twitter-hashtag-button' data-size='large'>Tweet ";
+    var proto_3 = "</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>";
+
+    var btn = proto_1 + ht.substr(1) + proto_2 + ht + proto_3;
+    var twitter_button = document.getElementById("twitterbutton");
+    twitter_button.innerHTML = "";
+    twitter_button.innerHTML = btn;
+}
+
 function entry() {
-    var hashtag = get_hashtag();    
-    hashtag_search(hashtag);
+    window.addEventListener("message", function(ev) {
+        console.log(ev.data);
+        if (ev.data != current_group) {
+            var group_name = ev.data.split(":");
+            var hashtag = "#" + group_name[group_name.length-1]; 
+            /* insert twitter button */
+            conjure_tweet_button(hashtag);
+            hashtag_search(hashtag);
+        } else {
+            clog("no changes required, same group.");
+        }
+    });
+
+    top.postMessage("let's go!", "http://portaldev.cloud.jiscadvance.biz"); 
 }
 
